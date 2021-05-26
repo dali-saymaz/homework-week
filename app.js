74ece2cf9e56345129b184af42d885fbbd96194c
unknown <dalidasy24@gmail.com>
2021-05-26 08:15:45 +0200

    week 5

--- app.js
+++ app.js
@@ -15,3 +15,23 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
+
+var btn1=document.querySelector("#btn1");
+var btn2=document.querySelector("#btn2");
+var btn3=document.querySelector("#btn3");
+var btn4=document.querySelector("#btn4");
+var kutu=document.querySelector("#kutu");
+
+btn1.onclick=function(){
+  kutu.style.background="#ff0000";//div zemin rengini kırmızı yap
+}
+btn2.onclick=function(){
+  kutu.style.color="#ffffff";//div yazı rengini beyaz yap
+}
+btn3.onclick=function(){
+  kutu.style.background="#ff4000";//div zemin rengini kırmızı yap
+}
+btn4.onclick=function(){
+  kutu.style.color="#fff5ff";//div yazı rengini beyaz yap
+}
